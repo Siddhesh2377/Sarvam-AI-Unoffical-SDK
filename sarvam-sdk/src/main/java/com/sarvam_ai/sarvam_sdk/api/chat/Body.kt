163204f83678acc07c3e251dcd05d60d760cdf87
home <darkwildhacker@gmail.com>
2025-06-21 12:54:43 +0530

    init

--- sarvam-sdk/src/main/java/com/sarvam_ai/sarvam_sdk/api/chat/Body.kt
+++ sarvam-sdk/src/main/java/com/sarvam_ai/sarvam_sdk/api/chat/Body.kt
@@ -1,11 +1,12 @@
 package com.sarvam_ai.sarvam_sdk.api.chat
 
+import com.sarvam_ai.sarvam_sdk.models.Models
 import com.squareup.moshi.JsonClass
 
 
 @JsonClass(generateAdapter = true)
 data class ChatRequest(
-    val model: String = "sarvam-m",  // You can change this to "sarvam-0.5" if that's the model you want
+    val model: String = Models.SARVAM_M.modelId,
     val stream: Boolean = true,
     val messages: List<Message>
 )
